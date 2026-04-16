610d983886c4fdf9f771c15b77d3c26315156455
Ian Kipchirchir Koech <iankipchirchir51@gmail.com>
2026-04-16 12:28:03 +0300

    add seed script for populating initial bird data

---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Bird(db.Model):
+    __tablename__ = "birds"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    species = db.Column(db.String(100), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "species": self.species
+        }
